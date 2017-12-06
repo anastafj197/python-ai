7e55e986cb74ffb7c5afee257b42e4ac692a30e9
anastafj197 <frankie.anastasia@gmail.com>
2017-12-05 19:50:56 -0500

    create web_scrape

---
+++ web_scrape.py
@@ -0,0 +1,25 @@
+# Frank Anastasia 
+# Web scrapping tool using beautiful soup 
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20cards'
+
+# opening connection, grabbing the page 
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# Does html parsing 
+page_soup = soup(page_html, "html.parser")
+
+# Grabs each product 
+containers = page_soup.findAll("div", {"class":"item-container"})
+
+# Loops through each item 
+for container in containers:
+	brand = container.div.div.a.img["title"]
+
+	title_container = container.findAll("a", {"class":"item-title"})
+	producttitle_container[0].text
