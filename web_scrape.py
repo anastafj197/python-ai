23efb1750a2ce9dace27739edc5cae00bf525c11
anastafj197 <frankie.anastasia@gmail.com>
2017-12-05 21:10:46 -0500

    wrote the scraped data to an excel file

--- web_scrape.py
+++ web_scrape.py
@@ -18,9 +18,30 @@ page_soup = soup(page_html, "html.parser")
 # Grabs each product 
 containers = page_soup.findAll("div", {"class":"item-container"})
 
+# The file to write scraped up stuff to 
+filename = "products.csv"
+f = open(filename, "w")
+
+headers = "brand, product_name, shipping\n"
+
+f.write(headers)
+
 # Loops through each item 
 for container in containers:
 	brand = container.div.div.a.img["title"]
 
 	title_container = container.findAll("a", {"class":"item-title"})
-	producttitle_container[0].text
+	product_name = title_container[0].text
+
+	shipping_container = container.findAll("li", {"class":"price-ship"})
+	shipping = shipping_container[0].text.strip()
+
+	print("brand: " + brand)
+	print("product_name: " + product_name)
+	print("shipping: " + shipping) 
+	#print()
+
+	f.write(brand + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+
+# If you dont close file you cant open the file 
+f.close()
