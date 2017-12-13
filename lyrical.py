436f7758e76864a7416dd70102c28035bf46702b
anastafj197 <frankie.anastasia@gmail.com>
2017-12-12 19:05:35 -0500

    Begin project
    Just a simple start

---
+++ lyrical.py
@@ -0,0 +1,14 @@
+# Frank Anastasia 
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://lyrics.az/grateful-dead/allsongs.html'
+
+# opening connection, grabbing the page 
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# Does html parsing 
+page_soup = soup(page_html, "html.parser")
