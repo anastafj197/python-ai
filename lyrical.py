f7c273e107727308c50eda8c06edec10b75dd9d8
anastafj197 <frankie.anastasia@gmail.com>
2017-12-12 20:21:52 -0500

    Update lyrical.py

--- lyrical.py
+++ lyrical.py
@@ -3,6 +3,8 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+from selenium import webdriver 
+
 my_url = 'https://lyrics.az/grateful-dead/allsongs.html'
 
 # opening connection, grabbing the page 
