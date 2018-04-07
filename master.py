96cb7615742873b5ec653020c58ac6896f190fe2
anastafj197 <frankie.anastasia@gmail.com>
2018-04-07 16:19:33 -0400

    Create master file
    Used to run the project cleanly

---
+++ master.py
@@ -0,0 +1,17 @@
+# File used to create tagging structure 
+import C_T_S
+
+# File used to decide a theme and generate a title
+import theme 
+
+# File used to generate chorus line 
+import gen_chorus
+
+# Main file where work is done 
+# Syllables, Rhymes, Synonyms, Parts of speach 
+import lyrical 
+
+# Input files varying in length used for testing 
+ex_file = "Fire_On_The_Mountain.txt"
+ex_file_2 = "Scarlet_Fire.txt"
+ex_file_3 = "Dead_Combo.txt"
