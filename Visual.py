78d5e114cfb3ab25f7e718215ff60835e8b8ece1
anastafj197 <frankie.anastasia@gmail.com>
2018-06-26 18:15:25 +0200

    Multiple turtles
    - color

--- Visual.py
+++ Visual.py
@@ -7,11 +7,28 @@ import turtle
 # Load the screen 
 loadWindow = turtle.Screen()
 
-# Basic loop to draw a square 
-for i in range (4):
-	turtle.forward(100)
-	turtle.left(90)
+boy0 = turtle.Pen()
+boy0.color("red")
 
+boy1 = turtle.Pen()
+boy1.color("orange")
+
+boy2 = turtle.Pen()
+boy2.color("yellow")
+
+boy3 = turtle.Pen()
+boy3.color("green")
+
+# Basic loop to draw a square 
+for i in range (40):
+	boy0.forward(100)
+	boy0.left(89)
+	boy1.forward(100)
+	boy1.left(79)
+	boy2.forward(100)
+	boy2.left(69)
+	boy3.forward(100)
+	boy3.left(59)
 
 # End condition 
 turtle.exitonclick()
