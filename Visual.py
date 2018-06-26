f34fc308a41e910de1e4159d19bdb6f668f7138c
anastafj197 <frankie.anastasia@gmail.com>
2018-06-26 17:40:13 +0200

    Learning the basics
    Graphics in the Turtle python package

---
+++ Visual.py
@@ -0,0 +1,17 @@
+# Visual project using turtle graphics
+# By Frank Anastasia
+
+# Import modules
+import turtle
+
+# Load the screen 
+loadWindow = turtle.Screen()
+
+# Basic loop to draw a square 
+for i in range (4):
+	turtle.forward(100)
+	turtle.left(90)
+
+
+# End condition 
+turtle.exitonclick()
