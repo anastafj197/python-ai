2453c954ed49bef5a8d1cf0f5bcce6c7d8b31f29
anastafj197 <frankie.anastasia@gmail.com>
2018-06-30 23:34:30 +0200

    Methods

--- Visual.py
+++ Visual.py
@@ -31,29 +31,42 @@ boy4.color("blue")
 boy4.speed(0)
 
 # Basic loop to draw a top mandala 
-for i in range (74):
-	boy0.forward(100)
-	boy0.left(99)
-	boy1.forward(100)
-	boy1.left(79)
-	boy2.forward(100)
-	boy2.left(69)
-	boy3.forward(100)
-	boy3.left(59)
-	boy4.forward(100)
-	boy4.left(49)
+def big():
+	for i in range (74):
+		boy0.forward(100)
+		boy0.left(99)
+		boy1.forward(100)
+		boy1.left(79)
+		boy2.forward(100)
+		boy2.left(69)
+		boy3.forward(100)
+		boy3.left(59)
+		boy4.forward(100)
+		boy4.left(49)
 
-for i in range (74):
-	boy0.forward(50)
-	boy0.right(99)
-	boy1.forward(50)
-	boy1.right(79)
-	boy2.forward(50)
-	boy2.right(69)
-	boy3.forward(50)
-	boy3.right(59)
-	boy4.forward(50)
-	boy4.right(49)
+def small():
+	for i in range (74):
+		boy0.forward(50)
+		boy0.right(99)
+		boy1.forward(50)
+		boy1.right(79)
+		boy2.forward(50)
+		boy2.right(69)
+		boy3.forward(50)
+		boy3.right(59)
+		boy4.forward(50)
+		boy4.right(49)
+
+big()
+small()
+
+boy0.forward(200)
+boy1.forward(200)
+boy2.forward(200)
+boy3.forward(200)
+boy4.forward(200)
+
+small()
 
 
 # turtle.update()
